00f8e28386ff5f5ed174c309f8c3f15cbf12ad2f
GiovannaFa <35294721+GiovannaFa@users.noreply.github.com>
2024-10-06 15:06:10 -0400

    Make pagination work for search in post_list (#13)

--- src/controllers/posts_list.js
+++ src/controllers/posts_list.js
@@ -9,69 +9,63 @@ const {escapeRegex, getPageRange} = require("../helpers/libs");
 const totalPages = 100;
 const pagesPerSet = 10;
 
+
 ctrl.index = async (req, res) => {
-    const url = req.originalUrl.split('?')[0];
+    const pagination = req.query.pagination ? parseInt(req.query.pagination): 4;
+    const currentPage = req.query.page ? parseInt(req.query.page): 1;
     if (req.query.search){
         const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-        Post.find({title: regex}, async function(err, posts){
-            if(err){
-                console.error(err)
-            } else{
-                let viewModel = { posts: [] };
-                viewModel.posts = posts;
-                viewModel = await sidebar(viewModel);
-                res.render('posts_list', viewModel);
-            }
-        }).sort({timestamp: -1}).lean()
-    } else {
-        const pagination = req.query.pagination ? parseInt(req.query.pagination): 1;
-        const currentPage = req.query.page ? parseInt(req.query.page): 1;
-        const startPage = Math.floor((currentPage - 1) / pagesPerSet) * pagesPerSet + 1;
-        const endPage = Math.min(startPage + pagesPerSet - 1, totalPages);
-        
-        const hasPreviousSet = startPage > 1;
-        const hasNextSet = endPage < totalPages;
-        const previousSetStart = Math.max(1, startPage - pagesPerSet);
-        const nextSetStart = Math.min(totalPages, endPage + 1);
-        const posts  = await Post.find()
+        var posts = await Post.find({title: regex})
         .skip((currentPage - 1) * pagination)
         .limit(pagination)
         .sort({timestamp: -1}).lean()
-        totalPosts = await Post.find()
-        const pages = Math.ceil(totalPosts.length/pagination)
-
-        count = getPageRange(pages, currentPage)
-
-        var posts_and_more = [];
-        for (let i in posts){
-            const writer = await Post.aggregate([{
-                $match:{_id: posts[i]._id}},
-                {
-                $lookup: {
-                    from: "users",
-                    localField: "user",
-                    foreignField: "_id",
-                    as: "usuario"}
-                }
-                ]);
-            const author = writer[0].usuario[0];
-            posts[i].author = author.name;
-            posts_and_more.push(posts[i])
-            };
-        let viewModel = { posts_and_more: [] };
-        
-        viewModel.posts = posts_and_more;
-        viewModel.currentPage = currentPage;
-        viewModel.pages = count;
-        viewModel.currentTotalPages = pages;
-        viewModel.hasPreviousSet = hasPreviousSet
-        viewModel.previousSetStart = previousSetStart
-        viewModel.hasNextSet = hasNextSet
-        viewModel.nextSetStart = nextSetStart
-        viewModel.url = url
-        viewModel = await sidebar(viewModel);
-        res.render('posts_list', viewModel);
+        var totalPosts = await Post.find({title: regex})
+    } else {
+        var posts  = await Post.find()
+        .skip((currentPage - 1) * pagination)
+        .limit(pagination)
+        .sort({timestamp: -1}).lean()
+        var totalPosts = await Post.find()
     }
+    const url = req.originalUrl.split('?')[0];
+    const startPage = Math.floor((currentPage - 1) / pagesPerSet) * pagesPerSet + 1;
+    const endPage = Math.min(startPage + pagesPerSet - 1, totalPages);
+    const hasPreviousSet = startPage > 1;
+    const hasNextSet = endPage < totalPages;
+    const previousSetStart = Math.max(1, startPage - pagesPerSet);
+    const nextSetStart = Math.min(totalPages, endPage + 1);
+    var pages = Math.ceil(totalPosts.length/pagination)
+    const count = getPageRange(pages, currentPage)
+
+    var viewModel = {};
+    var posts_and_more = [];
+    for (let i in posts){
+        const writer = await Post.aggregate([{
+            $match:{_id: posts[i]._id}},
+            {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "usuario"}
+            }
+            ]);
+        const author = writer[0].usuario[0];
+        posts[i].author = author.name;
+        posts_and_more.push(posts[i])
+        };
+    
+    viewModel.posts = posts_and_more;
+    viewModel.currentPage = currentPage;
+    viewModel.pages = count;
+    viewModel.currentTotalPages = pages;
+    viewModel.hasPreviousSet = hasPreviousSet
+    viewModel.previousSetStart = previousSetStart
+    viewModel.hasNextSet = hasNextSet
+    viewModel.nextSetStart = nextSetStart
+    viewModel.url = url
+    viewModel = await sidebar(viewModel);
+    res.render('posts_list', viewModel);
 };
 
 
@@ -81,7 +75,7 @@ ctrl.find_about = async (req, res) => {
     const startPage = Math.floor((currentPage - 1) / pagesPerSet) * pagesPerSet + 1;
     const endPage = Math.min(startPage + pagesPerSet - 1, totalPages);
     const concerning = req.params.about;
-    const posts = await Post.find({about: req.params.about})
+    const posts = await Post.find({about: concerning})
     .skip((currentPage - 1) * pagination)
     .limit(pagination)
     .sort({timestamp: -1}).lean();
@@ -95,6 +89,7 @@ ctrl.find_about = async (req, res) => {
     const previousSetStart = Math.max(1, startPage - pagesPerSet);
     const nextSetStart = Math.min(totalPages, endPage + 1);
 
+    var viewModel = {};
     var posts_and_more = [];
         for (let i in posts){
             const writer = await Post.aggregate([{
@@ -111,7 +106,6 @@ ctrl.find_about = async (req, res) => {
             posts[i].author = author.name;
             posts_and_more.push(posts[i])
             };
-        let viewModel = { posts_and_more: [] };
         
         viewModel.posts = posts_and_more;
         viewModel.currentPage = currentPage;
@@ -132,7 +126,7 @@ ctrl.find_where = async (req, res) => {
     const startPage = Math.floor((currentPage - 1) / pagesPerSet) * pagesPerSet + 1;
     const endPage = Math.min(startPage + pagesPerSet - 1, totalPages);
     const concerning = req.params.city;
-    const posts = await Post.find({where: req.params.city})
+    const posts = await Post.find({where: concerning})
     .skip((currentPage - 1) * pagination)
     .limit(pagination)
     .sort({timestamp: -1}).lean();
@@ -146,6 +140,7 @@ ctrl.find_where = async (req, res) => {
     const previousSetStart = Math.max(1, startPage - pagesPerSet);
     const nextSetStart = Math.min(totalPages, endPage + 1);
 
+    var viewModel = {};
     var posts_and_more = [];
         for (let i in posts){
             const writer = await Post.aggregate([{
@@ -162,7 +157,6 @@ ctrl.find_where = async (req, res) => {
             posts[i].author = author.name;
             posts_and_more.push(posts[i])
             };
-        let viewModel = { posts_and_more: [] };
         
         viewModel.posts = posts_and_more;
         viewModel.currentPage = currentPage;
@@ -184,7 +178,7 @@ ctrl.find_where_about = async (req, res) => {
     const endPage = Math.min(startPage + pagesPerSet - 1, totalPages);
     const city = req.params.city;
     const topic = req.params.topic;
-    const posts = await Post.find({where: req.params.city, about: req.params.topic})
+    const posts = await Post.find({where: req.params.city, about: topic})
     .skip((currentPage - 1) * pagination)
     .limit(pagination)
     .sort({timestamp: -1}).lean();
@@ -198,6 +192,7 @@ ctrl.find_where_about = async (req, res) => {
     const previousSetStart = Math.max(1, startPage - pagesPerSet);
     const nextSetStart = Math.min(totalPages, endPage + 1);
 
+    var viewModel = {};
     var posts_and_more = [];
         for (let i in posts){
             const writer = await Post.aggregate([{
@@ -214,7 +209,6 @@ ctrl.find_where_about = async (req, res) => {
             posts[i].author = author.name;
             posts_and_more.push(posts[i])
             };
-        let viewModel = { posts_and_more: [] };
         
         viewModel.posts = posts_and_more;
         viewModel.currentPage = currentPage;
